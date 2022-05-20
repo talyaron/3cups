3a0b1a94a77401245725b8ff4ef33f693c15e72c
Tal Yaron <tal.yaron@gmail.com>
2022-05-20 16:56:42 +0300

    basic game ready

--- dist/index.js
+++ dist/index.js
@@ -1,36 +1,61 @@
-var cups = document.querySelectorAll('.cup');
+var cups = document.querySelectorAll(".cup");
 var smile = document.querySelector("#smile");
 var target = false;
+var offsetX = 0, offsetY = 0;
+var win = new Audio('./img/win.wav');
+var loose = new Audio('./img/loose.wav');
+var body = document.body;
+body.ondrop = function () {
+};
+body.ondragover = function () {
+};
 cups.forEach(function (cup) {
-    cup.addEventListener('drop', function (ev) {
-        ev.preventDefault();
-        console.log('drop');
-        console.log(ev.target.id);
-        target = ev.target.id;
-        smile.style.display = 'none';
+    cup.addEventListener("dragstart", function (ev) {
+        var img = new Image();
+        img.src = "./img/small-ball.png";
+        offsetX = ev.x;
+        offsetY = ev.y;
+        ev.dataTransfer.setDragImage(img, 0, 0);
     });
-    cup.addEventListener('dragover', function (ev) {
+    cup.addEventListener("drop", function (ev) {
         ev.preventDefault();
-        console.log(ev.target.id);
-        ev.dataTransfer.dropEffect = "move";
+        if (target === false) {
+            target = ev.target.id;
+            smile.style.display = "none";
+            cups.forEach(function (cup) {
+                cup.style.opacity = "1";
+                cup.draggable = "true";
+            });
+        }
     });
-    cup.addEventListener('click', function (ev) {
-        if (ev.target.id === target) {
-            console.log('correct');
+    cup.addEventListener("dragover", function (ev) {
+        ev.preventDefault();
+    });
+    cup.addEventListener("dblclick", function (ev) {
+        if (target) {
+            if (ev.target.id === target) {
+                win.play();
+                body.style.backgroundColor = "green";
+            }
+            else {
+                loose.play();
+                body.style.backgroundColor = "red";
+            }
         }
-        else {
-            console.log('worng');
-        }
+    });
+    cup.addEventListener("drag", function (ev) {
+        ev.target.style.top = ev.y - 200 + "px";
+        ev.target.style.left = ev.x - 200 + "px";
     });
 });
-smile.addEventListener('drop', function (ev) {
+smile.addEventListener("drop", function (ev) {
     // ev.preventDefault();
     console.log(ev.target.id);
 });
-smile.addEventListener('dragstart', function (ev) {
-    ev.dataTransfer.dropEffect = "copy";
+smile.addEventListener("dragstart", function (ev) {
+    //   ev.dataTransfer.dropEffect = "move";
     // console.log(ev.target.id)
 });
-smile.addEventListener('drag', function (ev) {
+smile.addEventListener("drag", function (ev) {
     // console.log(ev.target.id)
 });
--- index.ts
+++ index.ts
@@ -1,42 +1,75 @@
-const cups = document.querySelectorAll('.cup');
-const smile:HTMLElement =document.querySelector("#smile");
-let target:string|false = false
+const cups = document.querySelectorAll(".cup");
+const smile: HTMLElement = document.querySelector("#smile");
+let target: string | false = false;
+let offsetX = 0,
+  offsetY = 0;
 
-cups.forEach(cup=> {
-    cup.addEventListener('drop',(ev:any)=>{
-        ev.preventDefault();
-        console.log('drop')
-        console.log(ev.target.id)
-        target = ev.target.id;
-        smile.style.display = 'none';
-        
-    })
-    cup.addEventListener('dragover',(ev:any)=>{
-        ev.preventDefault();
-        console.log(ev.target.id)
-        ev.dataTransfer.dropEffect = "move";
-    })
-    cup.addEventListener('click',(ev:any)=>{
-        if(ev.target.id === target){
-            console.log('correct')
-        } else{
-            console.log('worng')
-        }
-    })
+  const win = new Audio('./img/win.wav');
+  const loose = new Audio('./img/loose.wav');
+
+const body = document.body;
+body.ondrop = () => {
+
+};
+body.ondragover = () => {
+
+};
+
+cups.forEach((cup) => {
+  cup.addEventListener("dragstart", (ev: any) => {
+    const img = new Image();
+    img.src = "./img/small-ball.png";
+    offsetX = ev.x;
+    offsetY = ev.y;
+
+
+
+    ev.dataTransfer.setDragImage(img, 0, 0);
+  });
+  cup.addEventListener("drop", (ev: any) => {
+    ev.preventDefault();
+    if (target === false) {
+      target = ev.target.id;
+    
+      smile.style.display = "none";
+      cups.forEach((cup: any) => {
+        cup.style.opacity = "1";
+        cup.draggable = "true";
+      });
+    }
+  });
+  cup.addEventListener("dragover", (ev: any) => {
+    ev.preventDefault();
+  });
+  cup.addEventListener("dblclick", (ev: any) => {
+    if (target) {
+      if (ev.target.id === target) {
+        win.play()
+        body.style.backgroundColor = "green";
+      } else {
+          loose.play()
+        body.style.backgroundColor = "red";
+      }
+    }
+  });
+
+  cup.addEventListener("drag", (ev: any) => {
  
-})
+    ev.target.style.top = `${ev.y - 200}px`;
+    ev.target.style.left = `${ev.x - 200}px`;
+  });
+});
 
-smile.addEventListener('drop',(ev:any)=>{
-    // ev.preventDefault();
-    console.log(ev.target.id)
-})
+smile.addEventListener("drop", (ev: any) => {
+  // ev.preventDefault();
+  console.log(ev.target.id);
+});
 
-smile.addEventListener('dragstart',(ev:any)=>{
-    ev.dataTransfer.dropEffect = "copy";
-    // console.log(ev.target.id)
-})
+smile.addEventListener("dragstart", (ev: any) => {
+  //   ev.dataTransfer.dropEffect = "move";
+  // console.log(ev.target.id)
+});
 
-smile.addEventListener('drag',(ev:any)=>{
- 
-    // console.log(ev.target.id)
-})
+smile.addEventListener("drag", (ev: any) => {
+  // console.log(ev.target.id)
+});
